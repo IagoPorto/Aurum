d4d7925a238b02eef6fa951b2dcd4136eccae6e1
IagoPorto <iagopm8@gmail.com>
2024-10-12 11:18:16 +0200

    first step

---
+++ backend/core/aurum-core/src/main/java/com/aurum/aurum_core/AurumCoreApplication.java
@@ -0,0 +1,13 @@
+package com.aurum.aurum_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurumCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurumCoreApplication.class, args);
+	}
+
+}
---
+++ backend/core/aurum-core/src/test/java/com/aurum/aurum_core/AurumCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurum.aurum_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurumCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
